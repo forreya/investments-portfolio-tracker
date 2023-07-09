1d6fd5de6df503883b8a42a1660f3134062d9c9d
Ryan Lai <ryanlai12@gmail.com>
2023-07-09 20:54:31 +0100

    Created state for x values and y values & setup useEffect for stock API data fetching

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 
 const Stock = () => {
+  const [stockChartXValues, setStockChartXValues] = useState([]);
+  const [stockChartYValues, setStockChartYValues] = useState([]);
+
+  useEffect(() => {
+    fetchStock();
+  },[])
+
+  const fetchStock = () => {
+    // Fetch from API
+  }
+
   return (
     <div>
       <h1>Stock</h1>
